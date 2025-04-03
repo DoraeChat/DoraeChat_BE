61c36833baf549bd5ab9c9fe070563f7743bb400
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-04-03 15:59:31 +0700

    validate update channel

--- src/services/ChannelService.js
+++ src/services/ChannelService.js
@@ -10,18 +10,24 @@ const ChannelService = {
     const conversation = await Conversation.getById(channel.conversationId);
     if (!conversation) throw new Error("Conversation not found");
     if (!conversation.managerIds.includes(channel.memberId) || !conversation.leaderId === channel.memberId)
-      throw new Error("User is not a member of this conversation");
+      throw new Error("Member is not access to add channel");
     if (!conversation.type) throw new Error("Conversation is not a group");
     return await Channel.addChannel(channel);
   },
 
   async updateChannel(channelId, channel) {
+    const conversation = await Conversation.getById(channel.conversationId);
+    if (!channelId) throw new Error("Channel ID is required");
+    if (channel.memberId === conversation.leaderId || conversation.managerIds.includes(channel.memberId)) 
+      throw new Error("Member is not access to update channel");
+    if (!conversation.type) throw new Error("Conversation is not a group");
     return await Channel.updateChannel(channelId, channel);
   },
 
   async deleteChannel(channelId) {
     return await Channel.deleteChannel(channelId);
   },
+  
   async getById(channelId) {
     return await Channel.getById(channelId);
   },

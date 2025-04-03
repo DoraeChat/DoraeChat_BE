c12101ff7364116526e3c6a7b19c378909252ae7
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-04-03 15:52:53 +0700

    add validation add channel (conversation valid and is a group, member is leader or manager)

--- src/services/ChannelService.js
+++ src/services/ChannelService.js
@@ -1,4 +1,5 @@
 const Channel = require("../models/Channel");
+const Conversation = require("../models/Conversation");
 
 const ChannelService = {
   async getAllChannelByConversationId(conversationId) {
@@ -6,6 +7,11 @@ const ChannelService = {
   },
 
   async addChannel(channel) {
+    const conversation = await Conversation.getById(channel.conversationId);
+    if (!conversation) throw new Error("Conversation not found");
+    if (!conversation.managerIds.includes(channel.memberId) || !conversation.leaderId === channel.memberId)
+      throw new Error("User is not a member of this conversation");
+    if (!conversation.type) throw new Error("Conversation is not a group");
     return await Channel.addChannel(channel);
   },
 

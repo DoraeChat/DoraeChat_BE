5a22ac86c74370a13290c110d2137e996515b3be
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-04-04 12:03:13 +0700

    validate channel

--- src/services/ChannelService.js
+++ src/services/ChannelService.js
@@ -8,28 +8,40 @@ const ChannelService = {
 
   async addChannel(channel) {
     const conversation = await Conversation.getById(channel.conversationId);
+
     if (!conversation) throw new Error("Conversation not found");
-    if (!conversation.managerIds.includes(channel.memberId) || !conversation.leaderId === channel.memberId)
+
+    if (!conversation.managerIds.includes(channel.memberId) && conversation.leaderId.toString() !== channel.memberId)
       throw new Error("Member is not access to add channel");
+
     if (!conversation.type) throw new Error("Conversation is not a group");
+
     return await Channel.addChannel(channel);
   },
 
   async updateChannel(channelId, channel) {
     const conversation = await Conversation.getById(channel.conversationId);
+
     if (!channelId) throw new Error("Channel ID is required");
-    if (channel.memberId === conversation.leaderId || conversation.managerIds.includes(channel.memberId)) 
+
+    if (!conversation.managerIds.includes(channel.memberId) && conversation.leaderId.toString() !== channel.memberId) 
       throw new Error("Member is not access to update channel");
+
     if (!conversation.type) throw new Error("Conversation is not a group");
+
     return await Channel.updateChannel(channelId, channel);
   },
 
   async deleteChannel(channelId, memberId) {
-    const conversation = await Conversation.getById(channelId);
     if (!channelId) throw new Error("Channel ID is required");
-    if (memberId === conversation.leaderId || conversation.managerIds.includes(memberId)) 
+
+    const conversation = await Conversation.getById(channelId);
+
+    if (!conversation.managerIds.includes(memberId) && conversation.leaderId.toString() !== memberId) 
       throw new Error("Member is not access to update channel");
+
     if (!conversation.type) throw new Error("Conversation is not a group");
+
     return await Channel.deleteChannel(channelId);
   },
 

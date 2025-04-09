f11f38cb4dc648183266f58e3be93099e70dc984
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-04-09 10:01:45 +0700

    fix me routes

--- src/routes/MeRoutes.js
+++ src/routes/MeRoutes.js
@@ -5,8 +5,16 @@ const { upload } = require('../config/cloudinary');
 
 router.put('/profile', MeController.updateUser);
 router.get('/profile/:id', MeController.getById);
-router.put('/avatar', upload.single('avatar'), MeController.updateAvatarUser);
-router.put('/cover', upload.single('cover'), MeController.updateCoverUser);
+router.put('/avatar', (req, res, next) => {
+    // Lưu id vào req để sử dụng ở middleware
+    req.userId = req.params.id;
+    next();
+}, upload.single('avatar'), MeController.updateAvatarUser);
+router.put('/cover', (req, res, next) => {
+    // Lưu id vào req để sử dụng ở middleware
+    req.userId = req.params.id;
+    next();
+}, upload.single('cover'), MeController.updateCoverUser);
 router.put('/password', MeController.updatePassword);
 
 module.exports = router;    

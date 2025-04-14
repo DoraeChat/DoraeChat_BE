8773d780cc2c2e463c16c5347ef999adf718c58b
Kha <minhkha20031996@gmail.com>
2025-04-14 14:27:34 +0700

    oke

--- src/services/MessageService.js
+++ src/services/MessageService.js
@@ -86,7 +86,7 @@ class MessageService {
   async getMessagesByConversationId(
     conversationId,
     userId,
-    { skip = 0, limit = 20, beforeTimestamp = null } = {}
+    { skip = 0, limit = 100, beforeTimestamp = null } = {}
   ) {
     // 1. Validate conversation
     const conversation = await Conversation.findById(conversationId);

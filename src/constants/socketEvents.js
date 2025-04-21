164ed624d9c6cce3280048b5affa1f4d92067bfc
GGDuck <vunguyenminhduc1410@gmail.com>
2025-04-21 20:40:06 +0700

    Update socketEvents.js

--- src/constants/socketEvents.js
+++ src/constants/socketEvents.js
@@ -15,6 +15,9 @@ const SOCKET_EVENTS = {
   JOIN_CONVERSATIONS: "join-conversations",
   JOIN_CONVERSATION: "join-conversation",
   LEAVE_CONVERSATION: "leave-conversation",
+  DISBANDED_CONVERSATION: "disbanded-conversation",
+  NEW_GROUP_CONVERSATION: "new-group-conversation",
+
   //Messages
   RECEIVE_MESSAGE: "receive-message",
   MESSAGE_RECALLED: "message-recalled",

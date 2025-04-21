a106b8aac469306c6147ee5c5448f0b20f3d2dda
GGDuck <vunguyenminhduc1410@gmail.com>
2025-04-21 23:36:36 +0700

    Update callValidate.js

--- src/validates/callValidate.js
+++ src/validates/callValidate.js
@@ -14,7 +14,6 @@ async function validateCallPermission(conversationId, userId, channelId = null) 
             console.warn(`[CALL][NO_MEMBER] User ${userId} is not a member of conversation ${conversationId}`);
             return false;
         }
-
         if (!member.active) {
             console.warn(`[CALL][MEMBER_INACTIVE] User ${userId} is inactive in conversation ${conversationId}`);
             return false;
@@ -27,29 +26,29 @@ async function validateCallPermission(conversationId, userId, channelId = null) 
         }
 
 
-        if (conversation.type && !channelId) {
-            console.warn(`[CALL][CHANNEL_ID_REQUIRED] Channel ID is required for group conversations`);
-            return false;
+        // if (conversation.type && !channelId) {
+        //     console.warn(`[CALL][CHANNEL_ID_REQUIRED] Channel ID is required for group conversations`);
+        //     return false;
 
-        }
-        if (!conversation.type && channelId) {
-            console.warn(`[CALL][CHANNEL_ID_NOT_REQUIRED] Channel ID is not required for one-on-one conversations`);
-            return false;
-        }
+        // }
+        // if (!conversation.type && channelId) {
+        //     console.warn(`[CALL][CHANNEL_ID_NOT_REQUIRED] Channel ID is not required for one-on-one conversations`);
+        //     return false;
+        // }
 
-        // Nếu là group, kiểm tra channel
-        let validChannelId = null;
-        if (conversation.type) {
-            const channel = await Channel.findById(channelId);
-            if (
-                !channel ||
-                channel.conversationId.toString() !== conversationId.toString()
-            ) {
-                console.warn(`[CALL][CHANNEL_NOT_FOUND] Channel ${channelId} not found or does not belong to conversation ${conversationId}`);
-                return false;
-            }
-            validChannelId = channel._id;
-        }
+        // // Nếu là group, kiểm tra channel
+        // let validChannelId = null;
+        // if (conversation.type) {
+        //     const channel = await Channel.findById(channelId);
+        //     if (
+        //         !channel ||
+        //         channel.conversationId.toString() !== conversationId.toString()
+        //     ) {
+        //         console.warn(`[CALL][CHANNEL_NOT_FOUND] Channel ${channelId} not found or does not belong to conversation ${conversationId}`);
+        //         return false;
+        //     }
+        //     validChannelId = channel._id;
+        // }
 
         return true;
     } catch (error) {

ca8714214d442c0f83a5ce12fc9c1c01cc75fecf
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-05-01 10:33:27 +0700

    update classify

--- src/services/ClassifyService.js
+++ src/services/ClassifyService.js
@@ -1,112 +1,141 @@
-const Classify = require('../models/Classify');
-const Conversation = require('../models/Conversation');
-const Color = require('../models/Color');
-const User = require('../models/User');
-const NotFoundError = require('../exceptions/NotFoundError');
-const CustomError = require('../exceptions/CustomError');
-const mongoose = require("mongoose");
+const Classify = require("../models/Classify");
+const Conversation = require("../models/Conversation");
+const Color = require("../models/Color");
+const User = require("../models/User");
+const NotFoundError = require("../exceptions/NotFoundError");
+const CustomError = require("../exceptions/CustomError");
 
 const ClassifyService = {
-    async getAllByUserId(userId) {
-        return await Classify.getAllByUserId(userId);
-    },
+  async getAllByUserId(userId) {
+    const classifies = await Classify.getAllByUserId(userId);
+    const classifiesWithColor = await Promise.all(
+      classifies.map(async (classify) => {
+        const color = await Color.findById(classify.colorId).lean();
+        if (!color) throw new NotFoundError("Color");
 
-    async addClassify(classify) {
-        const userId = classify.userId;
-        const colorId = classify.colorId;
-        const conversationIds = classify.conversationIds;
+        return {
+          ...classify._doc,
+          color: {
+            _id: color._id,
+            name: color.name,
+            code: color.code,
+            type: color.type,
+          },
+        };
+      })
+    );
+    return classifiesWithColor;
+  },
 
-        if (!userId) throw new CustomError('User cannot empty', 400);
-        if (!colorId) throw new CustomError('Color cannot empty', 400);
-        if (!conversationIds) throw new CustomError('Conversation cannot empty', 400);
-        if (!Array.isArray(conversationIds)) throw new CustomError('Conversation must be an array', 400);
+  async addClassify(classify) {
+    const userId = classify.userId;
+    const colorId = classify.colorId;
+    const conversationIds = classify.conversationIds;
 
-        const user = await User.findById(userId).lean();
-        if (!user) throw new NotFoundError('User');
+    if (!userId) throw new CustomError("User cannot empty", 400);
+    if (!colorId) throw new CustomError("Color cannot empty", 400);
+    if (!conversationIds)
+      throw new CustomError("Conversation cannot empty", 400);
+    if (!Array.isArray(conversationIds))
+      throw new CustomError("Conversation must be an array", 400);
 
-        const color = await Color.findById(colorId).lean();
-        if (!color) throw new NotFoundError('Color');
+    const user = await User.findById(userId).lean();
+    if (!user) throw new NotFoundError("User");
 
-        const conversations = await Conversation.find({ _id: { $in: conversationIds } }).lean();
-        if (conversations.length !== conversationIds.length) {
-            throw new NotFoundError('Some conversations');
-        }
+    const color = await Color.findById(colorId).lean();
+    if (!color) throw new NotFoundError("Color");
 
-        return await Classify.addClassify(classify);
-    },
-
-    async updateClassify(classify, classifyId) {
-        const userId = classify.userId;
-        const colorId = classify.colorId;
-        const conversationIds = classify.conversationIds;
-
-        if (!userId) throw new CustomError('User cannot empty', 400);
-        if (!colorId) throw new CustomError('Color cannot empty', 400);
-        if (!conversationIds) throw new CustomError('Conversation cannot empty', 400);
-        if (!Array.isArray(conversationIds)) throw new CustomError('Conversation must be an array', 400);
-
-        const user = await User.findById(userId).lean();
-        if (!user) throw new NotFoundError('User');
-
-        const color = await Color.findById(colorId).lean();
-        if (!color) throw new NotFoundError('Color');
-
-        const conversations = await Conversation.find({ _id: { $in: conversationIds } }).lean();
-        if (conversations.length !== conversationIds.length) {
-            throw new NotFoundError('Some conversations');
-        }
-
-        return await Classify.updateClassify(classify, classifyId);
-    },
-
-    async deleteClassify(classifyId, userId) {
-        if (!userId) throw new CustomError('User cannot empty', 400);
-        if (!classifyId) throw new CustomError('Classify cannot empty', 400);
-
-        const classify = await Classify.findById(classifyId).lean();
-        if (!classify) throw new NotFoundError('Classify');
-
-        if (classify.userId.toString() !== userId) {
-            throw new CustomError('User not authorized to delete this classify', 403);
-        }
-
-        return await Classify.deleteClassify(classifyId);
-    },
-
-    async addConversationToClassify(classifyId, conversationId) {
-        if (!conversationId) throw new CustomError('Conversation cannot empty', 400);
-        const conversation = await Conversation.findById(conversationId).lean();
-        if (!conversation) throw new NotFoundError('Conversation');
-
-        const classify = await Classify.findById(classifyId).lean();
-        if (!classify) throw new NotFoundError('Classify');
-
-        if (classify.conversationIds.includes(conversationId)) {
-            throw new CustomError('Conversation already in classify', 400);
-        }
-
-        return await Classify.addConversationToClassify(classifyId, conversationId);
-    },
-
-    async removeConversationFromClassify(classifyId, conversationId) {
-        if (!conversationId) throw new CustomError('Conversation cannot empty', 400);
-        const conversation = await Conversation.findById(conversationId).lean();
-        if (!conversation) throw new NotFoundError('Conversation');
-
-        const classify = await Classify.findById(classifyId).lean();
-        if (!classify) throw new NotFoundError('Classify');
-
-        const conversationIds = classify.conversationIds.map((id) => id.toString());
-        if (!conversationIds.includes(conversationId)) {
-            throw new CustomError('Conversation not in classify', 400);
-        }
-
-        return await Classify.removeConversationFromClassify(classifyId, conversationId);
-    },
-
-    async getById(classifyId) {
-        return await Classify.getById(classifyId);
+    const conversations = await Conversation.find({
+      _id: { $in: conversationIds },
+    }).lean();
+    if (conversations.length !== conversationIds.length) {
+      throw new NotFoundError("Some conversations");
     }
+
+    return await Classify.addClassify(classify);
+  },
+
+  async updateClassify(classify, classifyId) {
+    const userId = classify.userId;
+    const colorId = classify.colorId;
+    const conversationIds = classify.conversationIds;
+
+    if (!userId) throw new CustomError("User cannot empty", 400);
+    if (!colorId) throw new CustomError("Color cannot empty", 400);
+    if (!conversationIds)
+      throw new CustomError("Conversation cannot empty", 400);
+    if (!Array.isArray(conversationIds))
+      throw new CustomError("Conversation must be an array", 400);
+
+    const user = await User.findById(userId).lean();
+    if (!user) throw new NotFoundError("User");
+
+    const color = await Color.findById(colorId).lean();
+    if (!color) throw new NotFoundError("Color");
+
+    const conversations = await Conversation.find({
+      _id: { $in: conversationIds },
+    }).lean();
+    if (conversations.length !== conversationIds.length) {
+      throw new NotFoundError("Some conversations");
+    }
+
+    return await Classify.updateClassify(classify, classifyId);
+  },
+
+  async deleteClassify(classifyId, userId) {
+    if (!userId) throw new CustomError("User cannot empty", 400);
+    if (!classifyId) throw new CustomError("Classify cannot empty", 400);
+
+    const classify = await Classify.findById(classifyId).lean();
+    if (!classify) throw new NotFoundError("Classify");
+
+    if (classify.userId.toString() !== userId) {
+      throw new CustomError("User not authorized to delete this classify", 403);
+    }
+
+    return await Classify.deleteClassify(classifyId);
+  },
+
+  async addConversationToClassify(classifyId, conversationId) {
+    if (!conversationId)
+      throw new CustomError("Conversation cannot empty", 400);
+    const conversation = await Conversation.findById(conversationId).lean();
+    if (!conversation) throw new NotFoundError("Conversation");
+
+    const classify = await Classify.findById(classifyId).lean();
+    if (!classify) throw new NotFoundError("Classify");
+
+    if (classify.conversationIds.includes(conversationId)) {
+      throw new CustomError("Conversation already in classify", 400);
+    }
+
+    return await Classify.addConversationToClassify(classifyId, conversationId);
+  },
+
+  async removeConversationFromClassify(classifyId, conversationId) {
+    if (!conversationId)
+      throw new CustomError("Conversation cannot empty", 400);
+    const conversation = await Conversation.findById(conversationId).lean();
+    if (!conversation) throw new NotFoundError("Conversation");
+
+    const classify = await Classify.findById(classifyId).lean();
+    if (!classify) throw new NotFoundError("Classify");
+
+    const conversationIds = classify.conversationIds.map((id) => id.toString());
+    if (!conversationIds.includes(conversationId)) {
+      throw new CustomError("Conversation not in classify", 400);
+    }
+
+    return await Classify.removeConversationFromClassify(
+      classifyId,
+      conversationId
+    );
+  },
+
+  async getById(classifyId) {
+    return await Classify.getById(classifyId);
+  },
 };
 
 module.exports = ClassifyService;

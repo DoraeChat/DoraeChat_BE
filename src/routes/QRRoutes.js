b21223c38d2518adc6d9a935fc31f0e4a7b648c6
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-05-21 20:46:05 +0700

    clean QR

--- src/routes/QRRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const QRController = require('../controllers/QRController');
-
-router.get('/user/:userId', QRController.generateQRUser);
-router.get('/group/:groupId', QRController.generateQRGroup); // groupId chinh la conversationId 
-
-module.exports = router;
